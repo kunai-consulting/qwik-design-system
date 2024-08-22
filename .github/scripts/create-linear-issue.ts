b8b11d7592c77ac551472390b4215d78571ffd4b
jack shelton <jack.shelton@kunaico.com>
2024-08-22 13:33:53 -0500

    feat: add release script to action

--- .github/scripts/create-linear-issue.ts
+++ .github/scripts/create-linear-issue.ts
@@ -4,22 +4,29 @@ import { LinearClient } from "@linear/sdk";
  * Uses the linear SDK. Documentation at:
  * https://developers.linear.app/docs/sdk/getting-started
  */
-async function createLinearIssue() {
+async function createLinearReleaseIssue() {
   const linearClient = new LinearClient({ apiKey: process.env.LINEAR_API_KEY });
 
   const team = await linearClient.team("QWIK");
   const project = await linearClient.project("af4d86af8e3f");
 
+  const prTitle = process.env.PR_TITLE || "No PR Title";
+  const prDescription = process.env.PR_DESCRIPTION || "No PR Description";
+
   const issue = await linearClient.createIssue({
     teamId: team.id,
-    title: "Test",
-    description: "This is a test issue Jack has created with the linear SDK.",
+    title: prTitle,
+    description: `
+      project: https://github.com/kunai-consulting/kunai-design-system
+
+      ${prDescription}
+    `,
     projectId: project.id,
   });
 
   return issue;
 }
 
-createLinearIssue().catch((error) => {
+createLinearReleaseIssue().catch((error) => {
   throw error;
 });
