1bbe92dff9273cd0f3f0d615af1d6584047520d5
jack shelton <jack.shelton@kunaico.com>
2024-08-21 21:47:55 -0500

    feat: successfully creates an issue to the releases project

--- .github/scripts/create-linear-issue.ts
+++ .github/scripts/create-linear-issue.ts
@@ -8,7 +8,7 @@ async function createLinearIssue() {
   const linearClient = new LinearClient({ apiKey: process.env.LINEAR_API_KEY });
 
   const team = await linearClient.team("QWIK");
-  const project = await linearClient.project("release-af4d86af8e3f");
+  const project = await linearClient.project("af4d86af8e3f");
 
   const issue = await linearClient.createIssue({
     teamId: team.id,
