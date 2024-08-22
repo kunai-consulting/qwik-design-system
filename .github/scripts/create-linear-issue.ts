c8dc8bd56293008c92345482c7e01ac6fb447998
jack shelton <jack.shelton@kunaico.com>
2024-08-21 21:41:58 -0500

    feat: initial linear automation

---
+++ .github/scripts/create-linear-issue.ts
@@ -0,0 +1,25 @@
+import { LinearClient } from "@linear/sdk";
+
+/**
+ * Uses the linear SDK. Documentation at:
+ * https://developers.linear.app/docs/sdk/getting-started
+ */
+async function createLinearIssue() {
+  const linearClient = new LinearClient({ apiKey: process.env.LINEAR_API_KEY });
+
+  const team = await linearClient.team("QWIK");
+  const project = await linearClient.project("release-af4d86af8e3f");
+
+  const issue = await linearClient.createIssue({
+    teamId: team.id,
+    title: "Test",
+    description: "This is a test issue Jack has created with the linear SDK.",
+    projectId: project.id,
+  });
+
+  return issue;
+}
+
+createLinearIssue().catch((error) => {
+  throw error;
+});
