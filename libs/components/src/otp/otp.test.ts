7a38ed97466a71f180f60ef8d6819654afe18191
jack shelton <jack.shelton@kunaico.com>
2024-12-11 19:39:44 -0600

    more tests

--- libs/components/src/otp/otp.test.ts
+++ libs/components/src/otp/otp.test.ts
@@ -45,4 +45,55 @@ test.describe("critical functionality", () => {
     await input.pressSequentially("12345");
     await expect(input).toHaveValue("1234");
   });
+
+  test(`
+      GIVEN an OTP control
+      WHEN typing a number and pressing the left arrow key
+      THEN the selected character should be replaced with the new number
+    `, async ({ page }) => {
+    const d = await setup(page, "hero");
+    const input = d.getInput();
+
+    await input.pressSequentially("123");
+    // arrow left on keyboard
+    await input.press("ArrowLeft");
+    await input.pressSequentially("1");
+    await expect(input).toHaveValue("121");
+  });
+
+  test(`
+      GIVEN an OTP control
+      WHEN making a range selection (shift + arrow left)
+      THEN the selected characters should be replaced with the new number
+    `, async ({ page }) => {
+    const d = await setup(page, "hero");
+    const input = d.getInput();
+
+    await input.pressSequentially("1234");
+    await expect(input).toHaveValue("1234");
+
+    await input.press("Shift+ArrowLeft");
+    await input.press("Shift+ArrowLeft");
+
+    await expect(d.getItemAt(2)).toHaveAttribute("data-highlighted");
+    await expect(d.getItemAt(3)).toHaveAttribute("data-highlighted");
+
+    await input.pressSequentially("1");
+    await expect(input).toHaveValue("121");
+  });
+
+  test(`
+      GIVEN an OTP control that is full and the last character is selected
+      WHEN typing a new number
+      THEN the new number should swap with the last character
+    `, async ({ page }) => {
+    const d = await setup(page, "hero");
+    const input = d.getInput();
+
+    // initial setup
+    await input.pressSequentially("1234");
+
+    await input.press("5");
+    await expect(input).toHaveValue("1235");
+  });
 });
