68762c481be97b61e46a5fcaaa55991c30b71679
jack shelton <jack.shelton@kunaico.com>
2024-12-12 12:36:32 -0600

    simplify

--- libs/components/src/otp/otp-item.tsx
+++ libs/components/src/otp/otp-item.tsx
@@ -10,20 +10,25 @@ import {
 } from "@builder.io/qwik";
 import { OTPContextId } from "./otp-context";
 
-interface ItemContextType {
-  index: number;
-}
 type OTPProps = {
   _index?: number;
 } & PropsOf<"div">;
 
-export const itemContextId = createContextId<ItemContextType>("qd-otp-item");
+export const itemContextId = createContextId("qd-otp-item");
 export const OtpItem = component$(({ _index = 0, ...props }: OTPProps) => {
   const context = useContext(OTPContextId);
   const itemRef = useSignal<HTMLInputElement>();
   useContextProvider(itemContextId, { index: _index });
   const itemValue = context.inputValueSig.value[_index] || "";
   const isLastItemSig = useComputed$(() => _index === context.numItemsSig.value - 1);
+
+  const isHighlightedSig = useComputed$(
+    () => context.isFocusedSig.value && context.currIndexSig.value === _index
+  );
+
+  const isLastItemHighlightedSig = useComputed$(
+    () => isLastItemSig.value && context.isLastItemSig.value && context.isFocusedSig.value
+  );
 
   if (_index === undefined) {
     throw new Error("Qwik UI: Otp Item must have an index. This is a bug in Qwik UI");
@@ -35,12 +40,7 @@ export const OtpItem = component$(({ _index = 0, ...props }: OTPProps) => {
       ref={itemRef}
       data-qds-otp-item={_index}
       data-highlighted={
-        (context.isFocusedSig.value && context.currIndexSig.value === _index) ||
-        (isLastItemSig.value &&
-          context.isLastItemSig.value &&
-          !context.isFocusedSig.value === false)
-          ? ""
-          : undefined
+        isHighlightedSig.value || isLastItemHighlightedSig.value ? "" : undefined
       }
     >
       {itemValue}
