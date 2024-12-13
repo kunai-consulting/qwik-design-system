34dd9f310f128d9829c262264cc80067ea753d11
jack shelton <jack.shelton@kunaico.com>
2024-12-13 16:56:25 -0600

    improved example

--- apps/docs/src/routes/otp/examples/hero.tsx
+++ apps/docs/src/routes/otp/examples/hero.tsx
@@ -1,18 +1,9 @@
 import { type PropsOf, component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
 import { Otp } from "@kunai-consulting/qwik-components";
 
-export const head: DocumentHead = {
-  title: "Qwik Design System",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik Design System"
-    }
-  ]
-};
-
 export default component$(() => {
+  const slots = Array.from({ length: 4 });
+
   return (
     <div class="mt-10 flex justify-center">
       <div class="flex flex-col items-center justify-center">
@@ -31,24 +22,15 @@ export default component$(() => {
 
         <Otp.Root class="flex flex-col items-center justify-center">
           <Otp.HiddenInput />
-
           <div class="otp-container flex flex-row justify-center gap-2">
-            {Array.from({ length: 4 }, (_, index) => {
-              const uniqueKey = `otp-${index}-${Date.now()}`;
-
-              return (
-                <Otp.Item
-                  key={uniqueKey}
-                  class={
-                    "h-9 w-10 border-2 text-center data-[highlighted]:border-blue-600 rounded data-[highlighted]:ring-blue-100  data-[highlighted]:ring-[3px] data-[highlighted]:pl-1 data-[highlighted]:pr-1 caret-blue-600"
-                  }
-                >
-                  <Otp.Caret class="text-blue-500 text-xl animate-blink-caret">
-                    |
-                  </Otp.Caret>
-                </Otp.Item>
-              );
-            })}
+            {slots.map((slot) => (
+              <Otp.Item
+                key={`otp-${slot}`}
+                class="h-9 w-10 border-2 text-center data-[highlighted]:border-blue-600 rounded data-[highlighted]:ring-blue-100 data-[highlighted]:ring-[3px] data-[highlighted]:pl-1 data-[highlighted]:pr-1 caret-blue-600"
+              >
+                <Otp.Caret class="text-blue-500 text-xl animate-blink-caret">|</Otp.Caret>
+              </Otp.Item>
+            ))}
           </div>
         </Otp.Root>
 
