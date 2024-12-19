addb05ec7096b7219ac9b05bc78e13595704c8b9
jack shelton <jack.shelton@kunaico.com>
2024-12-19 17:53:20 -0600

    add in other comps

--- libs/components/src/checklist/checklist-item.tsx
+++ libs/components/src/checklist/checklist-item.tsx
@@ -1,7 +1,11 @@
 import { component$, Slot, type PropsOf } from "@builder.io/qwik";
 import { Checkbox } from "..";
 
-export const ChecklistItem = component$((props: PropsOf<typeof Checkbox.Root>) => {
+type ChecklistItemProps = PropsOf<typeof Checkbox.Root> & {
+  _index: number;
+};
+
+export const ChecklistItem = component$((props: ChecklistItemProps) => {
   console.log(props._index);
 
   return (
--- libs/components/src/checklist/index.ts
+++ libs/components/src/checklist/index.ts
@@ -3,3 +3,6 @@ export { ChecklistItem as Item } from "./checklist-item";
 export { ChecklistItemLabel as ItemLabel } from "./checklist-item-label";
 export { ChecklistItemIndicator as ItemIndicator } from "./checklist-item-indicator";
 export { ChecklistItemTrigger as ItemTrigger } from "./checklist-item-trigger";
+export { ChecklistItemDescription as ItemDescription } from "./checklist-item-description";
+export { ChecklistHiddenInput as HiddenInput } from "./checklist-hidden-input";
+export { ChecklistErrorMessage as ErrorMessage } from "./checklist-error-message";
