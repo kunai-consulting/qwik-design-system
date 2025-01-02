4ea5380ca3739eab53f27bbebe7cf60ea98b68cb
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-01-02 15:12:13 -0600

    add in styles for the anatomy table

--- apps/docs/src/docs-widgets/anatomy-table/anatomy-table.tsx
+++ apps/docs/src/docs-widgets/anatomy-table/anatomy-table.tsx
@@ -1,27 +1,32 @@
 import { component$ } from "@builder.io/qwik";
 import { api } from "~/routes/checkbox/auto-api/api";
-import { MainHeading } from "../toc/toc";
+import { MainHeading, SubHeading } from "../toc/toc";
 
 export const AnatomyTable = component$(() => {
   return (
     <div class="my-4">
-      <MainHeading>Anatomy</MainHeading>
-      <table class="w-full">
-        <thead>
-          <tr>
-            <th class="text-left p-2 border-b">Part</th>
-            <th class="text-left p-2 border-b">Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {api.anatomy.map((item) => (
-            <tr key={item.name}>
-              <td class="p-2 border-b font-mono">{item.name}</td>
-              <td class="p-2 border-b">{item.description || "—"}</td>
+      <SubHeading class="mb-4">Anatomy</SubHeading>
+      <div class="rounded-md border-qwik-neutral-900 border">
+        <table class="w-full border-collapse text-sm">
+          <thead>
+            <tr class="border-b border-qwik-neutral-900 bg-qwik-neutral-950">
+              <th class="py-4 px-4 text-left font-medium">Part</th>
+              <th class="py-4 px-4 text-left font-medium">Description</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {api.anatomy.map((item) => (
+              <tr
+                key={item.name}
+                class="border-b last-of-type:border-b-0 border-qwik-neutral-900"
+              >
+                <td class="py-4 px-4 font-mono text-sm">{item.name}</td>
+                <td class="py-4 px-4">{item.description || "—"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 });
