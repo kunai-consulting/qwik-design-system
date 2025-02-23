33acf63d38029437c1f28ac794e6c1f5bb3b4e46
thejackshelton <me@jackshelton.com>
2025-02-23 12:51:50 -0600

    moving off of preflight

--- apps/docs/src/docs-widgets/sidebar/sidebar.tsx
+++ apps/docs/src/docs-widgets/sidebar/sidebar.tsx
@@ -9,13 +9,17 @@ export const Sidebar = component$(() => {
     <nav class="flex-col gap-4 sticky top-20 h-full hidden md:flex">
       {menu?.items?.map((section) => (
         <div key={section.text}>
-          <h5 class="mb-2 font-bold text-xl text-qwik-neutral-300">{section.text}</h5>
+          <h5 class="mb-2 font-bold text-xl text-qwik-neutral-300">
+            {section.text}
+          </h5>
           <ul class="flex flex-col">
             {section.items?.map((item) => (
               <li key={item.href} class="hover:bg-qwik-neutral-800 rounded-sm">
                 <a
                   href={item.href}
-                  class={`w-full h-full p-1 px-2 block ${loc.url.pathname === item.href ? "text-qwik-blue-400" : ""}`}
+                  class={`w-full h-full p-1 px-2 block ${
+                    loc.url.pathname === item.href ? "text-qwik-blue-400" : ""
+                  }`}
                 >
                   {item.text}
                 </a>
