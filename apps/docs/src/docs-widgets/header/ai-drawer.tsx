185ecf113892d2656500b435b52b015e03a28fc5
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-02-28 19:20:34 -0600

    animation

--- apps/docs/src/docs-widgets/header/ai-drawer.tsx
+++ apps/docs/src/docs-widgets/header/ai-drawer.tsx
@@ -5,11 +5,7 @@ export const AIDrawer = component$(() => {
   return (
     <Modal.Root class="group">
       <Modal.Trigger>AI</Modal.Trigger>
-      <Modal.Panel
-        class="inset-y-0 right-0 ml-auto h-full max-w-sm rounded-l-base border-l data-closing:slide-out-to-right data-open:slide-in-from-right fixed w-full bg-background p-6 text-foreground transition-all backdrop:brightness-50 backdrop:backdrop-blur-sm
-          data-closing:duration-300 data-open:duration-300 data-open:animate-in data-closing:animate-out
-          backdrop:data-closing:duration-300 backdrop:data-open:duration-300 backdrop:data-open:animate-in backdrop:data-closing:animate-out backdrop:data-closing:fade-out backdrop:data-open:fade-in"
-      >
+      <Modal.Panel class="ml-auto data-open:translate-x-0 translate-x-[100%] transition-all duration-300 h-full w-[300px] backdrop:backdrop-blur-sm data-open:backdrop:backdrop-brightness-50 data-open:backdrop:animate-fade-in data-closing:backdrop:animate-fade-out">
         CONTENT
       </Modal.Panel>
     </Modal.Root>
