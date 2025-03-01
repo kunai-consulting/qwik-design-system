12e033c26b5302c8cf84aaa58945c1a771f5d1ac
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-02-28 20:30:53 -0600

    change origin

--- apps/docs/src/docs-widgets/header/ai-drawer.tsx
+++ apps/docs/src/docs-widgets/header/ai-drawer.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { Modal } from "@qwik-ui/headless";
 import { APIReference } from "../docs-ai/api";
 import { DocsAI } from "../docs-ai/docs";
+import { Sparkles } from "../sparkles/sparkles";
 
 export const AIDrawer = component$(() => {
   return (
@@ -9,7 +10,8 @@ export const AIDrawer = component$(() => {
       <Modal.Trigger>AI</Modal.Trigger>
       <Modal.Panel class="ml-auto data-open:translate-x-0 translate-x-[100%] duration-300 h-full w-[300px] data-open:backdrop:backdrop-brightness-20 data-open:backdrop:animate-fade-in data-closing:backdrop:animate-fade-out  border-l border-neutral-800 bg-neutral-accent flex flex-col gap-4 px-4 pt-4">
         <p class="text-neutral-foreground">
-          Welcome to Auto API! I'm here to help you create awesome documentation.
+          Welcome to <Sparkles>Auto API!</Sparkles> I'm here to help you create awesome
+          documentation.
         </p>
         <APIReference />
         <DocsAI />
