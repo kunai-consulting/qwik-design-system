c0b9d371fc4c5c7f00b4b7d4b92e167f7ee98c60
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-03 21:54:22 -0600

    philosophy

--- apps/docs/src/docs-widgets/image/image.tsx
+++ apps/docs/src/docs-widgets/image/image.tsx
@@ -4,9 +4,11 @@ import { cn } from "~/utils/cn";
 export const Image = component$<PropsOf<"img">>(({ alt, ...props }) => {
   return (
     <img
-      {...props}
       alt={alt}
-      class={cn("rounded-md border border-neutral-600 mt-6", props.class)}
+      loading="lazy"
+      decoding="async"
+      class={cn("rounded-md border border-neutral-primary mt-6", props.class)}
+      {...props}
     />
   );
 });
