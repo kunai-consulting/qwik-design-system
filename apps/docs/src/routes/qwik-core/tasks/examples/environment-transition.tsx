10c7f280bd6990a1b58bc7de20c8aff8406fa4df
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-05 01:11:05 -0600

    task transition

---
+++ apps/docs/src/routes/qwik-core/tasks/examples/environment-transition.tsx
@@ -0,0 +1,28 @@
+import { component$, isServer, useSignal, useTask$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    const isClicked = useSignal(false);
+    const environmentText = useSignal("Hello from the server!");
+
+    useTask$(({ track }) => {
+        track(() => isClicked.value);
+
+        // If the task is running on the server, don't do anything
+        if (isServer) return;
+
+        environmentText.value = "Hello from the browser!";
+    })
+
+    console.log("Notice my log doesn't show up when you click!")
+
+    
+    return (
+        <>
+            <button onClick$={() => isClicked.value = true}>
+                Click me
+            </button>
+
+            <div>The text is: {environmentText.value}</div>
+        </>
+    )
+})
