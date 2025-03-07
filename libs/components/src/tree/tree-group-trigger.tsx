53d2f5a9b981ffdd054bcbb7d5c3ae2df8989477
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-07 12:54:25 -0600

    tree group trigger becomes a tree item

--- libs/components/src/tree/tree-group-trigger.tsx
+++ libs/components/src/tree/tree-group-trigger.tsx
@@ -1,12 +1,15 @@
 import { type Component, type PropsOf, Slot, component$ } from "@builder.io/qwik";
 import { CollapsibleTrigger } from "../collapsible/collapsible-trigger";
+import { TreeItem } from "./tree-item";
 
 export const TreeGroupTrigger: Component<PropsOf<typeof CollapsibleTrigger>> = component$(
   ({ ...props }) => {
     return (
-      <CollapsibleTrigger {...props}>
-        <Slot />
-      </CollapsibleTrigger>
+      <TreeItem asChild>
+        <CollapsibleTrigger {...props}>
+          <Slot />
+        </CollapsibleTrigger>
+      </TreeItem>
     );
   }
 );
