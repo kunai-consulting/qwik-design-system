4b79df286df96954d4e2fe06d54e160f4da36bd0
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-12 22:16:28 -0500

    fix focus order

--- libs/components/src/tree/tree-item-trigger.tsx
+++ libs/components/src/tree/tree-item-trigger.tsx
@@ -6,17 +6,16 @@ import {
   useContext
 } from "@builder.io/qwik";
 import { withAsChild } from "../as-child/as-child";
-import {
-  type CollapsibleTrigger,
-  CollapsibleTriggerBase
-} from "../collapsible/collapsible-trigger";
+import { CollapsibleTrigger } from "../collapsible/collapsible-trigger";
 
 export const TreeItemTriggerBase: Component<PropsOf<typeof CollapsibleTrigger>> =
   component$(({ ...props }) => {
     return (
-      <CollapsibleTriggerBase {...props}>
-        <Slot />
-      </CollapsibleTriggerBase>
+      <CollapsibleTrigger {...props} asChild>
+        <div>
+          <Slot />
+        </div>
+      </CollapsibleTrigger>
     );
   });
 
