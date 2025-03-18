86e237082baa6e0a99149d866798872824582a85
ross-kunaico <ross.acheson@kunaico.com>
2025-03-18 11:01:06 -0600

    Update to use CodeNotate from npm
    Co-authored-by: Ross Acheson <rossacheson@users.noreply.github.com>

--- apps/docs/code-notate.config.js
+++ apps/docs/code-notate.config.js
@@ -1,4 +1,4 @@
-/** @type {import('@kunai-consulting/auto-api-cli').AutoApiConfig} */
+/** @type {import('@kunai-consulting/code-notate').CodeNotateConfig} */
 export default {
   aiModel: "claude-3-5-sonnet-20241022",
   aiProvider: "anthropic",
