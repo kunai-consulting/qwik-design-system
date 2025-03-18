54855a5f22da535a595e6989c8731c8678194c1a
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-17 20:59:00 -0500

    indexing

---
+++ apps/docs/src/routes/contributing/indexing/examples/the-problem.tsx
@@ -0,0 +1,60 @@
+import {
+  component$,
+  createContextId,
+  type Signal,
+  Slot,
+  useContext,
+  useContextProvider,
+  useSignal,
+  useTask$
+} from "@builder.io/qwik";
+
+export default component$(() => {
+  const isRenderedSig = useSignal(false);
+
+  return (
+    <>
+      <button type="button" onClick$={() => (isRenderedSig.value = true)}>
+        render on the client
+      </button>
+      {isRenderedSig.value && (
+        <Root>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </Root>
+      )}
+    </>
+  );
+});
+
+export const dummyContextId = createContextId<RootContext>("dummy-context");
+
+type RootContext = {
+  count: number;
+};
+
+const Root = component$(() => {
+  const count = 0;
+
+  const context: RootContext = {
+    count
+  };
+
+  useContextProvider(dummyContextId, context);
+
+  return (
+    <div>
+      <Slot />
+    </div>
+  );
+});
+
+const Item = component$(() => {
+  const context = useContext(dummyContextId);
+
+  const index = context.count++;
+
+  return <div>Index: {index}</div>;
+});
