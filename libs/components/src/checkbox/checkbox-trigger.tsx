c09c5b4c2b68b5c83d695c337f49552b050b273d
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-18 21:55:36 -0500

    file with aschild

--- libs/components/src/checkbox/checkbox-trigger.tsx
+++ libs/components/src/checkbox/checkbox-trigger.tsx
@@ -14,6 +14,8 @@ import { Render } from "../render/render";
 import { checkboxContextId } from "./checkbox-context";
 type PublicCheckboxControlProps = PropsOf<"button">;
 
+// file with asChild
+
 /** Interactive trigger component that handles checkbox toggling */
 export const CheckboxTriggerBase = component$((props: PublicCheckboxControlProps) => {
   const context = useContext(checkboxContextId);
