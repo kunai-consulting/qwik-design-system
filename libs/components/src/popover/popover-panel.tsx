961808270ed9317a5a9b6a67584f803ae5ab567c
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-22 15:23:55 -0500

    note

--- libs/components/src/popover/popover-panel.tsx
+++ libs/components/src/popover/popover-panel.tsx
@@ -16,6 +16,11 @@ export const PopoverPanelBase = component$((props: PropsOf<"div">) => {
 
   const handleToggle$ = $((e: CorrectedToggleEvent) => {
     context.isOpenSig.value = e.newState === "open";
+
+    // If user changes signal externally, but then uses internal toggle, we need to reset the external toggle flag
+    if (context.isExternalToggleSig.value === false) {
+      context.isExternalToggleSig.value = true;
+    }
   });
 
   return (
