08c9c74e119d207b78afe1415768360b2ea197ae
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-03-22 11:33:20 -0500

    update to onToggle$

--- libs/components/src/popover/popover-panel.tsx
+++ libs/components/src/popover/popover-panel.tsx
@@ -1,4 +1,4 @@
-import { component$, type PropsOf, Slot, useContext } from "@builder.io/qwik";
+import { $, component$, type PropsOf, Slot, useContext } from "@builder.io/qwik";
 import { Render } from "../render/render";
 import { withAsChild } from "../as-child/as-child";
 import { popoverContextId } from "./popover-root";
@@ -6,8 +6,20 @@ import { popoverContextId } from "./popover-root";
 export const PopoverPanelBase = component$((props: PropsOf<"div">) => {
   const context = useContext(popoverContextId);
   const panelId = `${context.localId}-panel`;
+
+  const handleToggle$ = $(() => {
+    context.isOpenSig.value = !context.isOpenSig.value;
+  });
+
   return (
-    <Render popover="auto" id={panelId} ref={context.panelRef} fallback="div" {...props}>
+    <Render
+      popover="auto"
+      onToggle$={[handleToggle$, props.onToggle$]}
+      id={panelId}
+      ref={context.panelRef}
+      fallback="div"
+      {...props}
+    >
       <Slot />
     </Render>
   );
