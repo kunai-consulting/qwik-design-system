27ff6f57dfaef784de2d06a69660545a2d4c4583
alex <zaynet.ua@gmail.com>
2025-03-25 14:17:56 +0100

    Work on resizable component - initial version

---
+++ apps/docs/src/routes/base/resizable/examples/custom.tsx
@@ -0,0 +1,39 @@
+import {component$, useStyles$} from "@builder.io/qwik";
+import { Resizable } from "@kunai-consulting/qwik";
+import styles from "./resizable-custom.css?inline"
+
+export default component$(() => {
+  useStyles$(styles);
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '350px',
+        padding: '20px',
+        background: '#f5f5f5'
+    }}
+    >
+      <Resizable.Root orientation="horizontal">
+        <Resizable.Panel width={200}>
+          <div style={{ padding: '20px', color: 'black' }}>Left Panel</div>
+        </Resizable.Panel>
+
+        <Resizable.Handle />
+
+        <Resizable.Panel>
+          <Resizable.Root orientation="vertical">
+            <Resizable.Panel height={200}>
+              <div style={{ padding: '20px', color: "black" }}>Top Panel</div>
+            </Resizable.Panel>
+
+            <Resizable.Handle />
+
+            <Resizable.Panel>
+              <div style={{ padding: '20px', color: "black" }}>Bottom Panel</div>
+            </Resizable.Panel>
+          </Resizable.Root>
+        </Resizable.Panel>
+      </Resizable.Root>
+    </div>
+  );
+});
