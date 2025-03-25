ff40d17311cc891eedb93b7f70b927b746d5440c
alex <zaynet.ua@gmail.com>
2025-03-25 14:17:56 +0100

    Work on resizable component - initial version

--- libs/components/src/resizable/resizable-root.tsx
+++ libs/components/src/resizable/resizable-root.tsx
@@ -1,20 +1,51 @@
-import { type PropsOf, Slot, component$, useStyles$ } from "@builder.io/qwik";
+import {
+  type PropsOf,
+  Slot,
+  component$,
+  useStyles$,
+  useSignal,
+  useContextProvider,
+} from "@builder.io/qwik";
 import { withAsChild } from "../as-child/as-child";
 import { Render } from "../render/render";
+import { type ResizableContext, resizableContextId } from "./resizable-context";
 import styles from "./resizable.css?inline";
+
 type ResizableRootProps = {
-  orientation: "horizontal" | "vertical";
+  orientation?: "horizontal" | "vertical";
+  disabled?: boolean;
+  // onChange$?: (sizes: PanelSize[]) => void;
 } & PropsOf<"div">;
 
-export const ResizableRootBase = component$<ResizableRootProps>(
-  ({ orientation, onChange$ }) => {
-    useStyles$(styles);
-    return (
-      <Render fallback="div" data-qds-resizable-root>
-        <Slot />
-      </Render>
-    );
-  }
-);
+export const ResizableRootBase = component$<ResizableRootProps>((props) => {
+  useStyles$(styles);
+  const { orientation = "horizontal", disabled = false } = props;
+
+  const isDragging = useSignal(false);
+  const initialSizes = useSignal<{ [key: string]: number }>({});
+  const startPosition = useSignal<number | null>(null);
+
+  const context: ResizableContext = {
+    orientation: useSignal(orientation),
+    disabled: useSignal(disabled),
+    startPosition,
+    isDragging,
+    initialSizes,
+  };
+
+  useContextProvider(resizableContextId, context);
+
+  return (
+    <Render
+      fallback="div"
+      {...props}
+      data-qds-resizable-root
+      data-orientation={orientation}
+      data-disabled={disabled}
+    >
+      <Slot />
+    </Render>
+  );
+});
 
 export const ResizableRoot = withAsChild(ResizableRootBase);
