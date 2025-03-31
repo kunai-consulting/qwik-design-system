26bb37412d9f3d7cb71ba0fc27dec674106a3b62
alex <zaynet.ua@gmail.com>
2025-03-31 18:43:56 +0200

    Work on resizable component

--- apps/docs/src/routes/base/resizable/examples/custom.tsx
+++ apps/docs/src/routes/base/resizable/examples/custom.tsx
@@ -1,21 +1,21 @@
-import {component$, useStyles$} from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import { Resizable } from "@kunai-consulting/qwik";
-import styles from "./resizable-custom.css?inline"
+import styles from "./resizable-custom.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
   return (
     <div
       style={{
-        width: '100%',
-        height: '350px',
-        padding: '20px',
-        background: '#f5f5f5'
-    }}
+        width: "100%",
+        height: "350px",
+        padding: "20px",
+        background: "#f5f5f5"
+      }}
     >
       <Resizable.Root orientation="horizontal">
         <Resizable.Panel width={200}>
-          <div style={{ padding: '20px', color: 'black' }}>Left Panel</div>
+          <div style={{ padding: "20px", color: "black" }}>Left Panel</div>
         </Resizable.Panel>
 
         <Resizable.Handle />
@@ -23,13 +23,13 @@ export default component$(() => {
         <Resizable.Panel>
           <Resizable.Root orientation="vertical">
             <Resizable.Panel height={200}>
-              <div style={{ padding: '20px', color: "black" }}>Top Panel</div>
+              <div style={{ padding: "20px", color: "black" }}>Top Panel</div>
             </Resizable.Panel>
 
             <Resizable.Handle />
 
             <Resizable.Panel>
-              <div style={{ padding: '20px', color: "black" }}>Bottom Panel</div>
+              <div style={{ padding: "20px", color: "black" }}>Bottom Panel</div>
             </Resizable.Panel>
           </Resizable.Root>
         </Resizable.Panel>
--- apps/docs/src/routes/base/resizable/examples/vertical.tsx
+++ apps/docs/src/routes/base/resizable/examples/vertical.tsx
@@ -1,29 +1,28 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
 import { Resizable } from "@kunai-consulting/qwik";
-import styles from "./resizable-custom.css?inline"
+import styles from "./resizable-custom.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
   return (
-    <div style={{
-      width: '100%',
-      padding: '20px',
-      background: '#f5f5f5'
-    }}>
+    <div
+      style={{
+        width: "100%",
+        padding: "20px",
+        background: "#f5f5f5"
+      }}
+    >
       <Resizable.Root orientation="vertical">
-        <Resizable.Panel
-          height={200}
-          minHeight={50}
-        >
-          <div style={{ padding: '20px', color: "black" }}>Header</div>
+        <Resizable.Panel height={200} minHeight={50}>
+          <div style={{ padding: "20px", color: "black" }}>Header</div>
         </Resizable.Panel>
         <Resizable.Handle />
         <Resizable.Panel height={150}>
-          <div style={{ padding: '20px', color: "black" }}>Main Content</div>
+          <div style={{ padding: "20px", color: "black" }}>Main Content</div>
         </Resizable.Panel>
         <Resizable.Handle />
         <Resizable.Panel height={250}>
-          <div style={{ padding: '20px', color: "black" }}>Footer</div>
+          <div style={{ padding: "20px", color: "black" }}>Footer</div>
         </Resizable.Panel>
       </Resizable.Root>
     </div>
