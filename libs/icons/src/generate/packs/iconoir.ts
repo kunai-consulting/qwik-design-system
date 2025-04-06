35fd1a8d1c74792a22f07aadbb7715bc052a3cc9
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-06 11:57:01 -0500

    fix: iconoir and tabler extract

--- libs/icons/src/generate/packs/iconoir.ts
+++ libs/icons/src/generate/packs/iconoir.ts
@@ -2,18 +2,28 @@ import glob from "fast-glob";
 import { definePack } from "../define-pack";
 import { extractor } from "../extractor";
 
+const extractRegex = /^.*\/(?<variant>.+?)\/(?<name>.+?)\.svg/;
+
+function iconoirExtract(path: string) {
+  const { variant, name } = extractor(extractRegex)(path);
+  return {
+    name,
+    variant: variant === "solid" ? "solid" : undefined
+  };
+}
+
 export const iconoirPack = definePack({
   name: "Iconoir",
   prefix: "In",
-  variants: {},
-  defaultVariants: {},
+  variants: { variant: ["solid", "regular"] },
+  defaultVariants: { variant: "regular" },
   download: {
     zip: "https://github.com/iconoir-icons/iconoir/archive/refs/heads/main.zip",
     folder: "iconoir-main/icons"
   },
   contents: {
-    files: glob("download/Iconoir/*.svg"),
-    extract: extractor(/^.*\/(?<name>.+?)\.svg/)
+    files: glob("download/Iconoir/*/*.svg"),
+    extract: iconoirExtract
   },
   projectUrl: "https://iconoir.com/",
   license: "MIT",
--- libs/icons/src/generate/packs/tabler.ts
+++ libs/icons/src/generate/packs/tabler.ts
@@ -2,21 +2,31 @@ import glob from "fast-glob";
 import { definePack } from "../define-pack";
 import { extractor } from "../extractor";
 
+const extractRegex = /^.*\/(?<variant>.+?)\/(?<name>.+?)\.svg/;
+
+function tablerExtract(path: string) {
+  const { variant, name } = extractor(extractRegex)(path);
+  return {
+    name,
+    variant: variant === "filled" ? "filled" : "outline"
+  };
+}
+
 export const tablerIconsPack = definePack({
   name: "TablerIcons",
   prefix: "Tb",
-  variants: {},
-  defaultVariants: {},
+  variants: { variant: ["filled", "outline"] },
+  defaultVariants: { variant: "outline" },
   download: {
-    zip: "https://github.com/tabler/tabler-icons/archive/refs/heads/master.zip",
-    folder: "tabler-icons-master/icons"
+    zip: "https://github.com/tabler/tabler-icons/archive/refs/heads/main.zip",
+    folder: "tabler-icons-main/icons"
   },
   contents: {
-    files: glob("download/TablerIcons/*.svg"),
-    extract: extractor(/^.*\/(?<name>.+?)\.svg/)
+    files: glob("download/TablerIcons/*/*.svg"),
+    extract: tablerExtract
   },
   projectUrl: "https://tabler.io/",
   license: "MIT",
-  licenseUrl: "https://github.com/tabler/tabler-icons/blob/master/LICENSE",
+  licenseUrl: "https://github.com/tabler/tabler-icons/blob/main/LICENSE",
   coloring: "keep"
 });
