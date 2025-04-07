8ec7940166b8e40a0bd2526b79495de7ae49ac61
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-07 14:16:21 -0500

    remove click from collapsible trigger

--- libs/components/src/collapsible/collapsible-trigger.tsx
+++ libs/components/src/collapsible/collapsible-trigger.tsx
@@ -1,10 +1,4 @@
-import {
-	$,
-	type PropsOf,
-	Slot,
-	component$,
-	useContext,
-} from "@builder.io/qwik";
+import { type PropsOf, Slot, component$, useContext } from "@builder.io/qwik";
 import { withAsChild } from "../as-child/as-child";
 import { Render } from "../render/render";
 import { collapsibleContextId } from "./collapsible-root";
@@ -12,35 +6,29 @@ import { collapsibleContextId } from "./collapsible-root";
 // changing collapsible trigger
 
 export const CollapsibleTriggerBase = component$<PropsOf<"button">>(
-	({ onClick$, ...props }) => {
-		const context = useContext(collapsibleContextId);
-		const contentId = `${context.itemId}-content`;
-		const triggerId = `${context.itemId}-trigger`;
+  ({ onClick$, ...props }) => {
+    const context = useContext(collapsibleContextId);
+    const contentId = `${context.itemId}-content`;
+    const triggerId = `${context.itemId}-trigger`;
 
-		const handleClick$ = $(async () => {
-			if (context.isOpenSig.value && context.collapsible === false) return;
-			context.isOpenSig.value = !context.isOpenSig.value;
-		});
-
-		return (
-			<Render
-				{...props}
-				fallback="button"
-				id={triggerId}
-				ref={context.triggerRef}
-				disabled={context.disabled}
-				data-disabled={context.disabled ? "" : undefined}
-				aria-disabled={context.disabled ? "true" : "false"}
-				data-open={context.isOpenSig.value}
-				data-closed={!context.isOpenSig.value}
-				aria-expanded={context.isOpenSig.value}
-				aria-controls={contentId}
-				onClick$={[handleClick$, onClick$]}
-			>
-				<Slot />
-			</Render>
-		);
-	},
+    return (
+      <Render
+        {...props}
+        fallback="button"
+        id={triggerId}
+        ref={context.triggerRef}
+        disabled={context.disabled}
+        data-disabled={context.disabled ? "" : undefined}
+        aria-disabled={context.disabled ? "true" : "false"}
+        data-open={context.isOpenSig.value}
+        data-closed={!context.isOpenSig.value}
+        aria-expanded={context.isOpenSig.value}
+        aria-controls={contentId}
+      >
+        <Slot />
+      </Render>
+    );
+  }
 );
 
 export const CollapsibleTrigger = withAsChild(CollapsibleTriggerBase);
