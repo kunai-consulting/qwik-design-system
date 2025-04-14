0a97a1749b79f8d1c754c7ada91ce8919553b3d9
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-13 21:06:13 -0500

    fix: vitest understands path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "pathe";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@kunai-consulting/qwik-utils": resolve(__dirname, "libs/utils/src"),
+      "@kunai-consulting/qwik-components": resolve(__dirname, "libs/components/src"),
+      "@kunai-consulting/qwik-icons": resolve(__dirname, "libs/icons/src")
+    }
+  },
   test: {
     include: ["**/*.unit.ts"]
   }
