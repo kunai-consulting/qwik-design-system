187f61bdd86c940e5ccc0085f809db0692bb47a1
ross-kunaico <ross.acheson@kunaico.com>
2025-04-18 09:53:47 -0400

    Enable setting default date
    Co-authored-by: Ross Acheson <rossacheson@users.noreply.github.com>

--- libs/components/src/date-input/utils.ts
+++ libs/components/src/date-input/utils.ts
@@ -1,9 +1,13 @@
-import type { DateFormat, LocalDate, Separator } from "../calendar/types";
+import type { DateFormat, ISODate, LocalDate, Separator } from "../calendar/types";
 import { MAX_YEAR, MIN_YEAR } from "./constants";
 import type { DateSegment } from "./types";
 
 export const getLocalDate = (date: Date) => {
   return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}` as LocalDate;
+};
+
+export const getISODate = (date: Date) => {
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}` as ISODate;
 };
 
 export const getSeparatorFromFormat = (format?: DateFormat): Separator => {
@@ -15,10 +19,11 @@ export const getSeparatorFromFormat = (format?: DateFormat): Separator => {
 
 export const getSegmentsFromFormat = (
   format: DateFormat,
-  separator: Separator
+  separator: Separator,
+  defaultDate?: ISODate
 ): DateSegment[] => {
-  const segments = format.split(separator);
-  return segments.map((segment) => {
+  const sections = format.split(separator);
+  const placeholderSegments = sections.map((segment) => {
     const type = segment.includes("y") ? "year" : segment.includes("d") ? "day" : "month";
     return {
       placeholderText: segment,
@@ -28,4 +33,24 @@ export const getSegmentsFromFormat = (
       max: type === "year" ? MAX_YEAR : type === "month" ? 12 : 31
     } as DateSegment;
   });
+  if (defaultDate) {
+    const [year, month, day] = defaultDate.split("-");
+    return placeholderSegments.map((segment) => {
+      const type = segment.type;
+      return {
+        ...segment,
+        isPlaceholder: false,
+        numericValue: type === "year" ? +year : type === "month" ? +month : +day,
+        displayValue: type === "year" ? year : type === "month" ? month : day,
+        max: type === "day" ? getLastDayOfMonth(+year, +month) : segment.max
+      } as DateSegment;
+    });
+  }
+  return placeholderSegments;
+};
+
+export const getLastDayOfMonth = (year: number, month: number) => {
+  // The Date constructor month is 0-indexed, but we can use our 1-based month value along with the 0 day value,
+  // which 'underflows' to the last day of the target month
+  return new Date(year, month, 0).getDate();
 };
