7d30da97cfb2701151fd4fa43a55c6cfcb096941
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-19 19:01:10 -0500

    feat: initial icons release

--- apps/docs/src/routes/icons/examples/icon-example.tsx
+++ apps/docs/src/routes/icons/examples/icon-example.tsx
@@ -1,6 +1,12 @@
+// import { component$ } from "@builder.io/qwik";
+// import { Lucide } from "@kunai-consulting/qwik-icons";
+
+// export default component$(() => {
+//   return <Lucide.BrainCog class="bg-red-500" />;
+// });
+
 import { component$ } from "@builder.io/qwik";
-import { Lucide } from "@kunai-consulting/qwik-icons";
 
 export default component$(() => {
-  return <Lucide.BrainCog class="bg-red-500" />;
+  return <div>Hello</div>;
 });
