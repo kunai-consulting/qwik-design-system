1f2d3496b3c8b48df9c20307332cf9386e8e79db
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-20 15:26:40 -0500

    feat: docs on internalRef

--- apps/docs/src/routes/contributing/composition/examples/render.tsx
+++ apps/docs/src/routes/contributing/composition/examples/render.tsx
@@ -1,17 +1,34 @@
-import { type PropsOf, Slot, component$ } from "@builder.io/qwik";
+import { $, type PropsOf, Slot, component$, useSignal } from "@builder.io/qwik";
 import { Render } from "@kunai-consulting/qwik";
 
 export default component$(() => {
+  const tooltipRef = useSignal<HTMLButtonElement>();
+
   return (
-    <TooltipTrigger onClick$={() => alert("Tooltip Trigger")}>
+    <TooltipTrigger
+      ref={tooltipRef}
+      onClick$={() => console.log("The consumer's ref", tooltipRef.value)}
+    >
       Tooltip Trigger
     </TooltipTrigger>
   );
 });
 
 export const TooltipTrigger = component$((props: PropsOf<"button">) => {
+  const triggerRef = useSignal<HTMLElement | undefined>();
+
+  const handleAlert$ = $(() => {
+    alert("Tooltip Trigger");
+    console.log("The library author's ref", triggerRef.value);
+  });
+
   return (
-    <Render fallback="button" {...props}>
+    <Render
+      fallback="button"
+      internalRef={triggerRef}
+      {...props}
+      onClick$={[handleAlert$, props.onClick$]}
+    >
       <Slot />
     </Render>
   );
