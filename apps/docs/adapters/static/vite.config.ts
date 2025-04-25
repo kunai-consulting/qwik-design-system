8734db6beffd59521fd50fa72e2a6ee30f5115b5
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-25 15:57:41 -0500

    add origin

--- apps/docs/adapters/static/vite.config.ts
+++ apps/docs/adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://qwik-design-system.pages.dev"
+        origin: "https://qwik.design"
       })
     ]
   };
