b03799707529be3ce01d5379deb27c3ba00d4937
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-04-25 22:10:44 -0500

    feat: initial toggle impl

---
+++ libs/components/src/toggle/toggle-root.tsx
@@ -0,0 +1,18 @@
+// import { component$, type PropsOf, Slot } from "@builder.io/qwik";
+// import { withAsChild } from "../as-child/as-child";
+// import { Render } from "../render/render";
+// import { useBindings } from "@kunai-consulting/qwik-utils";
+
+// type ToggleRootProps = PropsOf<"button">;
+
+// export const ToggleRootBase = component$((props: ToggleRootProps) => {
+//   const { pressedSig: isPressedSig } = useBi
+
+//   return (
+//     <Render fallback="button" {...props}>
+//       <Slot />
+//     </Render>
+//   );
+// });
+
+// export const ToggleRoot = withAsChild(ToggleRootBase);
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,2 +1,4 @@
 export * from "./resolver";
 export * from "./symbol";
+export * from "./bindings";
+export * from "./bound-signal";
