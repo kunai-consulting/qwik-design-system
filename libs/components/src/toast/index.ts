e76f3cd07f459e56e992204849243a0d9ba7b019
Robert Perez <robert.perez@kunaico.com>
2025-05-01 00:52:33 -0700

    Creates and adds component context

--- libs/components/src/toast/index.ts
+++ libs/components/src/toast/index.ts
@@ -1,1 +1,4 @@
 export { ToasterRoot as Root } from "./toaster-root";
+
+export { toastContextId } from "./toast-context";
+export type { Toast, ToastContext, ToastType } from "./toast-context";
---
+++ libs/components/src/toast/toast-context.ts
@@ -0,0 +1,23 @@
+import { type Signal, createContextId } from "@builder.io/qwik";
+
+export type ToastType = "success" | "error" | "info" | "warning";
+
+export type Toast = {
+  id: string;
+  type?: ToastType;
+  title?: string;
+  description?: string;
+  duration?: number;
+  dismissible?: boolean;
+};
+
+export type ToastContext = {
+  localId: string;
+  currentToast: Signal<Toast | null>;
+  defaultDuration: Signal<number>;
+  pauseOnHover: Signal<boolean>;
+  show$: (toast: Omit<Toast, "id">) => void;
+  hide$: () => void;
+};
+
+export const toastContextId = createContextId<ToastContext>("qds-toast");
