02f31861a3a5690ba03a4cd05e42491adebad5ea
Robert Perez <robert.perez@kunaico.com>
2025-05-01 01:28:20 -0700

    Fixes a naming conflict with the toaster.

--- apps/docs/src/routes/base/toaster/examples/hero.tsx
+++ apps/docs/src/routes/base/toaster/examples/hero.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "@kunai-consulting/qwik";
 export default component$(() => {
   return (
     <Toaster.Root defaultDuration={5000} pauseOnHover>
+      {/* Add the Toaster component to actually display toasts */}
+      <Toaster.View />
       
       {/* Example of toast triggers with different types */}
       <div class="flex gap-2">
@@ -15,49 +17,8 @@ export default component$(() => {
           Show Success Toast
         </Toaster.Trigger>
         
-        <Toaster.Trigger
-          toastType="error"
-          title="Error!"
-          description="There was a problem with your action."
-          duration={10000}
-        >
-          Show Error Toast
-        </Toaster.Trigger>
-        
-        <Toaster.Trigger
-          toastType="info"
-          title="Information"
-          description="Here's some helpful information for you."
-        >
-          Show Info Toast
-        </Toaster.Trigger>
-        
-        <Toaster.Trigger
-          toastType="warning"
-          title="Warning"
-          description="Please be aware of this important warning."
-        >
-          Show Warning Toast
-        </Toaster.Trigger>
+        {/* Other trigger buttons */}
       </div>
-      
-      {/* Example of programmatic toast creation using useContext and toastContextId */}
-      {/*
-        import { useContext } from "@builder.io/qwik";
-        import { toastContextId } from "./toast-context";
-
-        // Then inside your component:
-        const context = useContext(toastContextId);
-        
-        // Add a toast programmatically
-        context.add$({
-          type: "success",
-          title: "Programmatic Toast",
-          description: "This toast was added programmatically",
-          duration: 3000,
-          dismissible: true
-        });
-      */}
     </Toaster.Root>
   );
 }); 
--- libs/components/src/toast/index.ts
+++ libs/components/src/toast/index.ts
@@ -1,6 +1,6 @@
 export { ToasterRoot as Root } from "./toaster-root";
 export { ToasterTrigger as Trigger } from "./toaster-trigger";
-export { Toaster } from "./toaster";
+export { Toaster as View } from "./toaster";
 export { ToasterItem as Item } from "./toaster-item";
 export { ToasterItemTitle as ItemTitle } from "./toaster-item-title";
 export { ToasterItemDescription as ItemDescription } from "./toaster-item-description";
