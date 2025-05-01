40e4bacbe1a511d8830ef395c4ee3d673a12a70a
Robert Perez <robert.perez@kunaico.com>
2025-05-01 02:00:58 -0700

    adds css for hero

--- apps/docs/src/routes/base/toaster/examples/hero.tsx
+++ apps/docs/src/routes/base/toaster/examples/hero.tsx
@@ -1,23 +1,50 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import { Toaster } from "@kunai-consulting/qwik";
+import styles from "./toaster-custom.css?inline";
 
 export default component$(() => {
+  useStyles$(styles);
+
   return (
     <Toaster.Root defaultDuration={5000} pauseOnHover>
-      {/* Add the Toaster component to actually display toasts */}
       <Toaster.View />
       
-      {/* Example of toast triggers with different types */}
-      <div class="flex gap-2">
+      <div class="flex flex-wrap gap-2">
         <Toaster.Trigger
           toastType="success"
           title="Success!"
           description="Your action has been completed successfully."
+          class="success"
         >
-          Show Success Toast
+          Success Toast
         </Toaster.Trigger>
         
-        {/* Other trigger buttons */}
+        <Toaster.Trigger
+          toastType="error"
+          title="Error!"
+          description="There was a problem with your action."
+          class="error"
+        >
+          Error Toast
+        </Toaster.Trigger>
+        
+        <Toaster.Trigger
+          toastType="info"
+          title="Information"
+          description="Here's some helpful information for you."
+          class="info"
+        >
+          Info Toast
+        </Toaster.Trigger>
+        
+        <Toaster.Trigger
+          toastType="warning"
+          title="Warning"
+          description="Please be aware of this important warning."
+          class="warning"
+        >
+          Warning Toast
+        </Toaster.Trigger>
       </div>
     </Toaster.Root>
   );
