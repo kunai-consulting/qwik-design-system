3ebcf08da1096462b920f4c7b4fdede776d1a22c
ross-kunaico <ross.acheson@kunaico.com>
2025-05-02 13:00:24 -0600

    Update variable name for consistency
    Co-authored-by: Ross Acheson <rossacheson@users.noreply.github.com>

--- apps/docs/src/routes/base/date-input/examples/reactive.tsx
+++ apps/docs/src/routes/base/date-input/examples/reactive.tsx
@@ -4,27 +4,27 @@ import { DateInput } from "@kunai-consulting/qwik";
 
 export default component$(() => {
   useStyles$(styles);
-  const boundDate = useSignal<DateInput.ISODate | null>("1999-12-31");
+  const selectedDate = useSignal<DateInput.ISODate | null>("1999-12-31");
 
   return (
     <div class="date-input-container">
-      <DateInput.Root class="date-input-root-col" bind:date={boundDate}>
+      <DateInput.Root class="date-input-root-col" bind:date={selectedDate}>
         <DateInput.Label>Party like it's:</DateInput.Label>
         <DateInput.DateEntry />
       </DateInput.Root>
       <p>
-        Bound date: <span class="external-value">{boundDate.value}</span>
+        Bound date: <span class="external-value">{selectedDate.value}</span>
       </p>
       <div class="date-input-button-container">
         <button
-          onClick$={() => (boundDate.value = "2099-12-31")}
+          onClick$={() => (selectedDate.value = "2099-12-31")}
           type="button"
           class="set-value-button"
         >
           Set to 2099-12-31
         </button>
         <button
-          onClick$={() => (boundDate.value = null)}
+          onClick$={() => (selectedDate.value = null)}
           type="button"
           class="set-null-button"
         >
