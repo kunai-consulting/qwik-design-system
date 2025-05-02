b19991f7273b3e9e3318c07a2a82b5b5bcd0f312
alex <zaynet.ua@gmail.com>
2025-05-02 13:58:33 +0200

    DS-428 Dropdown component. Fix build, add initial API

--- libs/components/src/dropdown/dropdown-trigger.tsx
+++ libs/components/src/dropdown/dropdown-trigger.tsx
@@ -1,11 +1,11 @@
 import { type PropsOf, Slot, component$, useContext } from "@builder.io/qwik";
 import { withAsChild } from "../as-child/as-child";
-import { PopoverAnchorBase } from "../popover/popover-anchor";
 import { popoverContextId } from "../popover/popover-root";
+import { PopoverTriggerBase } from "../popover/popover-trigger";
 import { dropdownContextId } from "./dropdown-context";
 
 export type PublicDropdownTriggerProps = Omit<
-  PropsOf<typeof PopoverAnchorBase>,
+  PropsOf<typeof PopoverTriggerBase>,
   "popovertarget"
 >;
 /** Button that triggers the dropdown menu to open/close */
@@ -14,20 +14,20 @@ export const DropdownTriggerBase = component$<PublicDropdownTriggerProps>((props
   const popoverContext = useContext(popoverContextId);
 
   return (
-    <PopoverAnchorBase
+    <PopoverTriggerBase
       popovertarget={context.contentId}
       id={context.triggerId}
       aria-haspopup="menu"
       aria-expanded={context.isOpenSig.value}
       aria-controls={context.isOpenSig.value ? context.contentId : undefined}
-      ref={popoverContext.anchorRef}
+      ref={popoverContext.triggerRef}
       // The identifier for the dropdown trigger button
       data-qds-dropdown-trigger
       type="button"
       {...props}
     >
       <Slot />
-    </PopoverAnchorBase>
+    </PopoverTriggerBase>
   );
 });
 
