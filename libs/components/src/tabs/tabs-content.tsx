0c3826082d593f89362cbb3eccfd2a580a021fc7
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-05-16 17:00:57 -0500

    hide tab content

--- libs/components/src/tabs/tabs-content.tsx
+++ libs/components/src/tabs/tabs-content.tsx
@@ -1,15 +1,39 @@
-import { type PropsOf, Slot, component$ } from "@builder.io/qwik";
+import {
+  type PropsOf,
+  Slot,
+  component$,
+  useComputed$,
+  useContext
+} from "@builder.io/qwik";
 import { getNextIndex } from "@kunai-consulting/qwik-utils";
 import { withAsChild } from "../as-child/as-child";
 import { Render } from "../render/render";
+import { tabsContextId } from "./tabs-root";
 
 export type TabsContentProps = PropsOf<"div"> & {
   _index?: number;
+  value: string;
 };
 
 export const TabsContentBase = component$((props: TabsContentProps) => {
+  const context = useContext(tabsContextId);
+
+  const isVisibleSig = useComputed$(() => {
+    const isIndexBased = Number.parseInt(context.selectedValueSig.value) === props._index;
+
+    const isValueBased = props.value === context.selectedValueSig.value;
+
+    return isIndexBased || isValueBased;
+  });
+
   return (
-    <Render data-qds-tabs-content role="tabpanel" fallback="div" {...props}>
+    <Render
+      data-qds-tabs-content
+      role="tabpanel"
+      fallback="div"
+      hidden={!isVisibleSig.value}
+      {...props}
+    >
       <Slot />
     </Render>
   );
