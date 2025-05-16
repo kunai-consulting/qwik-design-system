94fe504a85cad7e7c9f1b25077fccee82077332b
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-05-16 18:11:51 -0500

    mouse functionality

--- libs/components/src/tabs/tabs.test.ts
+++ libs/components/src/tabs/tabs.test.ts
@@ -5,7 +5,7 @@ import { createTestDriver } from "./tabs.driver";
 async function setup(page: Page, exampleName: string) {
   await page.goto(`http://localhost:6174/base/tabs/${exampleName}`);
   const driver = createTestDriver(page);
-  return driver;
+  return { driver };
 }
 
 test.describe("critical functionality", () => {
@@ -21,6 +21,56 @@ test.describe("critical functionality", () => {
 
     expect(initialResults.violations).toEqual([]);
   });
+
+  test(`GIVEN a tabs component
+        WHEN rendered
+        THEN the first tab should be active
+`, async ({ page }) => {
+    const { driver: d } = await setup(page, "hero");
+
+    await expect(d.getTriggerAt(0)).toHaveAttribute("aria-selected", "true");
+    await expect(d.getContentAt(1)).toHaveAttribute("aria-selected", "false");
+  });
+
+  test(`GIVEN a tabs component
+        WHEN rendered
+        THEN the first content should be visible
+`, async ({ page }) => {
+    const { driver: d } = await setup(page, "hero");
+
+    await expect(d.getContentAt(0)).toBeVisible();
+    await expect(d.getContentAt(1)).toBeHidden();
+  });
+
+  test(`GIVEN a tabs component
+        WHEN clicking a new tab
+        THEN the selected tab should be updated
+`, async ({ page }) => {
+    const { driver: d } = await setup(page, "hero");
+
+    await expect(d.getTriggerAt(0)).toHaveAttribute("aria-selected", "true");
+    await expect(d.getTriggerAt(1)).toHaveAttribute("aria-selected", "false");
+
+    await d.getTriggerAt(1).click();
+
+    await expect(d.getTriggerAt(0)).toHaveAttribute("aria-selected", "false");
+    await expect(d.getTriggerAt(1)).toHaveAttribute("aria-selected", "true");
+  });
+
+  test(`GIVEN a tabs component
+        WHEN clicking a new tab
+        THEN the tabs content should be updated
+`, async ({ page }) => {
+    const { driver: d } = await setup(page, "hero");
+
+    await expect(d.getContentAt(0)).toBeVisible();
+    await expect(d.getContentAt(1)).toBeHidden();
+
+    await d.getTriggerAt(1).click();
+
+    await expect(d.getContentAt(0)).toBeHidden();
+    await expect(d.getContentAt(1)).toBeVisible();
+  });
 });
 
 test.describe("keyboard navigation", () => {
@@ -28,7 +78,7 @@ test.describe("keyboard navigation", () => {
         WHEN focus is inside the tablist
         THEN only the active tab should be focusable
 `, async ({ page }) => {
-    const d = await setup(page, "hero");
+    const { driver: d } = await setup(page, "hero");
 
     await expect(d.getTriggerAt(0)).toHaveAttribute("tabindex", "0");
     await expect(d.getTriggerAt(1)).toHaveAttribute("tabindex", "-1");
