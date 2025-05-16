85416027397102f27df3384a6baf7cae3013b30e
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-05-16 16:31:37 -0500

    register triggers

--- libs/components/src/tabs/tabs-root.tsx
+++ libs/components/src/tabs/tabs-root.tsx
@@ -1,11 +1,35 @@
-import { type PropsOf, Slot, component$ } from "@builder.io/qwik";
+import {
+  type PropsOf,
+  type Signal,
+  Slot,
+  component$,
+  createContextId,
+  useContextProvider,
+  useSignal
+} from "@builder.io/qwik";
 import { resetIndexes } from "@kunai-consulting/qwik-utils";
 import { withAsChild } from "../as-child/as-child";
 import { Render } from "../render/render";
 
 export type TabsRootProps = PropsOf<"div">;
 
+type TriggerRef = Signal<HTMLButtonElement | undefined>;
+
+export const tabsContextId = createContextId<TabsContext>("qds-tabs");
+
+type TabsContext = {
+  triggerRefs: Signal<TriggerRef[]>;
+};
+
 export const TabsRootBase = component$((props: TabsRootProps) => {
+  const triggerRefs = useSignal<TriggerRef[]>([]);
+
+  const context: TabsContext = {
+    triggerRefs
+  };
+
+  useContextProvider(tabsContextId, context);
+
   return (
     <Render data-qds-tabs-root fallback="div" {...props}>
       <Slot />
--- libs/components/src/tabs/tabs-trigger.tsx
+++ libs/components/src/tabs/tabs-trigger.tsx
@@ -1,13 +1,31 @@
-import { type PropsOf, Slot, component$ } from "@builder.io/qwik";
+import {
+  type PropsOf,
+  Slot,
+  component$,
+  useContext,
+  useSignal,
+  useTask$
+} from "@builder.io/qwik";
 import { getNextIndex } from "@kunai-consulting/qwik-utils";
 import { withAsChild } from "../as-child/as-child";
 import { Render } from "../render/render";
+import { tabsContextId } from "./tabs-root";
 
 export type TabsTriggerProps = PropsOf<"button"> & {
   _index?: number;
 };
 
 export const TabsTriggerBase = component$((props: TabsTriggerProps) => {
+  const triggerRef = useSignal<HTMLButtonElement>();
+  const context = useContext(tabsContextId);
+
+  useTask$(function setIndexOrder() {
+    const index = props._index;
+    if (index === undefined) return;
+
+    context.triggerRefs.value[index] = triggerRef;
+  });
+
   return (
     <Render data-qds-tabs-trigger role="tab" fallback="button" {...props}>
       <Slot />
