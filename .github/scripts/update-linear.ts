0be2ba90f4e30a552c8d8c9148e901b29a834e2a
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-05-20 11:56:21 -0500

    add a main function

--- .github/scripts/update-linear.ts
+++ .github/scripts/update-linear.ts
@@ -1,29 +1,30 @@
 import { LinearClient } from "@linear/sdk";
 import { changesetData, convertChangesetToMarkdown } from "./changeset-md";
 
-const linearClient = new LinearClient({
-  apiKey: process.env.LINEAR_API_KEY
-});
+async function main() {
+  const linearClient = new LinearClient({
+    apiKey: process.env.LINEAR_API_KEY
+  });
 
-const team = await linearClient.team("QWIK");
-const project = await linearClient.project("af4d86af8e3f");
+  const team = await linearClient.team("QWIK");
+  const project = await linearClient.project("af4d86af8e3f");
 
-const githubUrl = "https://github.com/kunai-consulting/qwik-design-system";
-const repoName = "Qwik Design System";
-const linkedPrUrl = `${githubUrl}/pull/${process.env.PR_NUMBER}`;
+  const githubUrl = "https://github.com/kunai-consulting/qwik-design-system";
+  const repoName = "Qwik Design System";
+  const linkedPrUrl = `${githubUrl}/pull/${process.env.PR_NUMBER}`;
 
-const issueTitle = process.env.PR_TITLE || "No PR Title";
-const existingIssues = await linearClient.issues({
-  filter: {
-    team: { id: { eq: team.id } },
-    project: { id: { eq: project.id } },
-    title: { eq: issueTitle }
-  }
-});
+  const issueTitle = process.env.PR_TITLE || "No PR Title";
+  const existingIssues = await linearClient.issues({
+    filter: {
+      team: { id: { eq: team.id } },
+      project: { id: { eq: project.id } },
+      title: { eq: issueTitle }
+    }
+  });
 
-export const existingIssue = existingIssues.nodes[0];
+  const existingIssue = existingIssues.nodes[0];
 
-const issueDescription = `
+  const issueDescription = `
 ## Release Summary
 
 This issue tracks the changes for the upcoming release of the ${repoName}.
@@ -34,75 +35,78 @@ ${convertChangesetToMarkdown(changesetData)}
 
 `;
 
-/**
- * Uses the linear SDK. Documentation at:
- * https://developers.linear.app/docs/sdk/getting-started
- */
-async function createLinearReleaseIssue() {
-  if (existingIssues.nodes.length > 0) {
-    console.log(`Issue with title "${issueTitle}" already exists.`);
-    return;
+  /**
+   * Uses the linear SDK. Documentation at:
+   * https://developers.linear.app/docs/sdk/getting-started
+   */
+  async function createLinearReleaseIssue() {
+    if (existingIssues.nodes.length > 0) {
+      console.log(`Issue with title "${issueTitle}" already exists.`);
+      return;
+    }
+
+    // user that created the API
+    const me = await linearClient.viewer;
+
+    // Find the "In Progress" state
+    const teamStates = await team.states();
+    const inProgressState = teamStates.nodes.find(
+      (state) => state.name.toLowerCase() === "in progress"
+    );
+    const inProgressStateId = inProgressState?.id ?? undefined;
+
+    const issuePayload = await linearClient.createIssue({
+      teamId: team.id,
+      title: issueTitle,
+      description: issueDescription,
+      projectId: project.id,
+      priority: 1,
+      assigneeId: me.id,
+      stateId: inProgressStateId
+    });
+
+    const issueId = (await issuePayload.issue)?.id;
+
+    if (!issueId) {
+      throw new Error("Update Linear: Issue Id needed for attachement to be creted.");
+    }
+
+    await linearClient.createAttachment({
+      issueId: issueId,
+      url: linkedPrUrl ?? "",
+      title: "GitHub Pull Request",
+      subtitle: issueTitle
+    });
+
+    return issuePayload;
   }
 
-  // user that created the API
-  const me = await linearClient.viewer;
+  // when we add new changesets before the release
+  async function updateLinearReleaseIssue() {
+    const updatedIssuePayload = await linearClient.updateIssue(existingIssue.id, {
+      description: issueDescription
+    });
 
-  // Find the "In Progress" state
-  const teamStates = await team.states();
-  const inProgressState = teamStates.nodes.find(
-    (state) => state.name.toLowerCase() === "in progress"
-  );
-  const inProgressStateId = inProgressState?.id ?? undefined;
+    // this for sure works, if we can't find the id in createLinearReleaseIssue
+    // await linearClient.createAttachment({
+    //   issueId: existingIssue.id,
+    //   url: linkedPrUrl ?? "",
+    //   title: "GitHub Pull Request",
+    //   subtitle: issueTitle,
+    // });
 
-  const issuePayload = await linearClient.createIssue({
-    teamId: team.id,
-    title: issueTitle,
-    description: issueDescription,
-    projectId: project.id,
-    priority: 1,
-    assigneeId: me.id,
-    stateId: inProgressStateId
-  });
-
-  const issueId = (await issuePayload.issue)?.id;
-
-  if (!issueId) {
-    throw new Error("Update Linear: Issue Id needed for attachement to be creted.");
+    return updatedIssuePayload;
   }
 
-  await linearClient.createAttachment({
-    issueId: issueId,
-    url: linkedPrUrl ?? "",
-    title: "GitHub Pull Request",
-    subtitle: issueTitle
-  });
-
-  return issuePayload;
-}
-
-// when we add new changesets before the release
-async function updateLinearReleaseIssue() {
-  const updatedIssuePayload = await linearClient.updateIssue(existingIssue.id, {
-    description: issueDescription
-  });
-
-  // this for sure works, if we can't find the id in createLinearReleaseIssue
-  // await linearClient.createAttachment({
-  //   issueId: existingIssue.id,
-  //   url: linkedPrUrl ?? "",
-  //   title: "GitHub Pull Request",
-  //   subtitle: issueTitle,
-  // });
-
-  return updatedIssuePayload;
-}
-
-try {
-  if (existingIssue) {
-    await updateLinearReleaseIssue();
-  } else {
-    await createLinearReleaseIssue();
+  try {
+    if (existingIssue) {
+      await updateLinearReleaseIssue();
+    } else {
+      await createLinearReleaseIssue();
+    }
+  } catch (e) {
+    console.error("Error from creating linear ticket script: ", e);
   }
-} catch (e) {
-  console.error("Error from creating linear ticket script: ", e);
 }
+
+main().catch(console.error);
