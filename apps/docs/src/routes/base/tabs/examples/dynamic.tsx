025a75ee31fd4f42ab8723145ac22c4131024408
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-05-20 10:09:30 -0500

    dynamic example

---
+++ apps/docs/src/routes/base/tabs/examples/dynamic.tsx
@@ -0,0 +1,52 @@
+import { component$, useComputed$, useSignal, useStyles$ } from "@builder.io/qwik";
+import { Tabs } from "@kunai-consulting/qwik";
+import tabsStyles from "./tabs.css?inline";
+
+export default component$(() => {
+  useStyles$(tabsStyles);
+
+  const count = useSignal(3);
+
+  const tabs = useComputed$(() => {
+    return Array.from({ length: count.value }, (_, i) => `Tab ${i + 1}`);
+  });
+
+  return (
+    <>
+      <Tabs.Root class="tabs-root">
+        <Tabs.List>
+          {tabs.value.map((tab) => (
+            <Tabs.Trigger class="tabs-trigger" key={tab}>
+              {tab}
+            </Tabs.Trigger>
+          ))}
+        </Tabs.List>
+        {tabs.value.map((tab) => (
+          <Tabs.Content key={tab}>Content {tab}</Tabs.Content>
+        ))}
+      </Tabs.Root>
+
+      <br />
+
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <button
+          type="button"
+          onClick$={() => {
+            count.value++;
+          }}
+        >
+          Add Tab
+        </button>
+
+        <button
+          type="button"
+          onClick$={() => {
+            count.value--;
+          }}
+        >
+          Remove Tab
+        </button>
+      </div>
+    </>
+  );
+});
