b011fd8e781ba5db123693b103160af4194c5514
ross-kunaico <ross.acheson@kunaico.com>
2025-05-27 10:35:32 -0600

    Minor refactors for readability
    Co-authored-by: Ross Acheson <rossacheson@users.noreply.github.com>

--- apps/docs/src/routes/base/date-input/examples/root-change.tsx
+++ apps/docs/src/routes/base/date-input/examples/root-change.tsx
@@ -1,34 +1,45 @@
 import { $, component$, useSignal, useStyles$ } from "@builder.io/qwik";
-
 import { DateInput } from "@kunai-consulting/qwik";
 
 export default component$(() => {
   useStyles$(styles);
 
   const secretDate = new Date("2001-01-01").getTime();
-  const datesSig = useSignal<(DateInput.ISODate | null)[]>([]);
-  const resultTextSig = useSignal<string | null>(null);
+  const dates = useSignal<(DateInput.ISODate | null)[]>([]);
+  const resultText = useSignal<string | null>(null);
 
-  const handleChange$ = $((dates: (DateInput.ISODate | null)[]) => {
-    datesSig.value = dates;
-    const [firstGuess, secondGuess] = dates;
-    if (firstGuess && secondGuess) {
-      const firstGuessDistance = Math.abs(new Date(firstGuess).getTime() - secretDate);
-      const secondGuessDistance = Math.abs(new Date(secondGuess).getTime() - secretDate);
-      if (firstGuessDistance === 0) {
-        resultTextSig.value = "The first date is correct!";
-      } else if (secondGuessDistance === 0) {
-        resultTextSig.value = "The second date is correct!";
-      } else if (firstGuessDistance === secondGuessDistance) {
-        resultTextSig.value = "The dates are equidistant from my date";
-      } else if (firstGuessDistance < secondGuessDistance) {
-        resultTextSig.value = "The first date is closer";
-      } else {
-        resultTextSig.value = "The second date is closer";
+  const getGuessResult = $(
+    (firstGuess: DateInput.ISODate | null, secondGuess: DateInput.ISODate | null) => {
+      if (firstGuess && secondGuess) {
+        const firstGuessDistance = Math.abs(new Date(firstGuess).getTime() - secretDate);
+        const secondGuessDistance = Math.abs(
+          new Date(secondGuess).getTime() - secretDate
+        );
+
+        if (firstGuessDistance === 0) {
+          return "The first date is correct!";
+        }
+        if (secondGuessDistance === 0) {
+          return "The second date is correct!";
+        }
+        if (firstGuessDistance === secondGuessDistance) {
+          return "The dates are equidistant from my date";
+        }
+        if (firstGuessDistance < secondGuessDistance) {
+          return "The first date is closer";
+        }
+        return "The second date is closer";
       }
-    } else {
-      resultTextSig.value = null;
+
+      // We don't have two dates to compare
+      return null;
     }
+  );
+
+  const handleChange$ = $(async (newDates: (DateInput.ISODate | null)[]) => {
+    dates.value = newDates;
+    const [firstGuess, secondGuess] = newDates;
+    resultText.value = await getGuessResult(firstGuess, secondGuess);
   });
 
   return (
@@ -47,7 +58,9 @@ export default component$(() => {
             <DateInput.Separator separator="-" />
             <DateInput.Day showLeadingZero={true} />
           </DateInput.DateEntry>
+
           <DateInput.Separator separator="||" />
+
           <DateInput.DateEntry data-second-entry>
             <DateInput.Year />
             <DateInput.Separator separator="-" />
@@ -59,10 +72,10 @@ export default component$(() => {
       </DateInput.Root>
 
       <div>
-        Dates: <span class="external-value">{JSON.stringify(datesSig.value)}</span>
+        Dates: <span class="external-value">{JSON.stringify(dates.value)}</span>
       </div>
 
-      <div>Result: {resultTextSig.value}</div>
+      <div>Result: {resultText.value}</div>
     </form>
   );
 });
