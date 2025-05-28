138f8f1d3e74ee079f28fbafbfdcc4dfeec4e765
thejackshelton-kunaico <167572331+thejackshelton-kunaico@users.noreply.github.com>
2025-05-28 11:47:47 -0500

    fix: switch (#219)
    * fix: switch
    
    * early retrun
    
    * fix: format
    
    ---------
    
    Co-authored-by: Oleksandr Zainetdinov <zaynet.ua@gmail.com>

--- libs/components/src/switch/switch-label.tsx
+++ libs/components/src/switch/switch-label.tsx
@@ -7,13 +7,16 @@ type PublicCheckboxLabelProps = PropsOf<typeof LabelBase>;
 /** Label component for the switch that can be clicked to toggle the state */
 const SwitchLabelBase = component$((props: PublicCheckboxLabelProps) => {
   const context = useContext(switchContextId);
+  const labelId = `${context.localId}-label`;
+  const triggerId = `${context.localId}-trigger`;
+
   return (
     <LabelBase
       {...props}
-      id={context.labelId}
+      id={labelId}
       // The identifier for the switch label element
       data-qds-switch-label
-      for={context.triggerId}
+      for={triggerId}
     >
       <Slot />
     </LabelBase>
