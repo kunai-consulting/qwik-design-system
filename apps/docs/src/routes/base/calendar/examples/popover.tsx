8c50cf20dce85ef91a387febd08396364441967b
ross-kunaico <ross.acheson@kunaico.com>
2025-06-05 14:06:10 -0600

    Refining built-in and example styles
    Co-authored-by: Ross Acheson <rossacheson@users.noreply.github.com>

--- apps/docs/src/routes/base/calendar/examples/popover.tsx
+++ apps/docs/src/routes/base/calendar/examples/popover.tsx
@@ -1,12 +1,30 @@
-import { component$ } from "@builder.io/qwik";
-
+import { $, component$, useSignal, useStyles$ } from "@builder.io/qwik";
 import { Calendar } from "@kunai-consulting/qwik";
 
+import styles from "./calendar.css?inline";
+
 export default component$(() => {
+  useStyles$(styles);
+  const selectedDate = useSignal<`${number}-${number}-${number}`>();
   return (
-    <Calendar.Root mode="popover">
+    <Calendar.Root mode="popover" class="calendar-root">
       <Calendar.Trigger>Trigger</Calendar.Trigger>
-      <Calendar.Content>Popover Calendar</Calendar.Content>
+      <Calendar.Content>
+        <Calendar.Header class="calendar-header">
+          <Calendar.Previous class="flex w-[20px] h-[20px] items-center justify-center border border-white rounded-md" />
+          <Calendar.Title />
+          <Calendar.Next class="flex w-[20px] h-[20px] items-center justify-center border border-white rounded-md" />
+        </Calendar.Header>
+        <Calendar.Grid class="w-full items-center">
+          <Calendar.GridDay
+            class="size-10 data-[selected]:border data-[current]:border-dashed data-[current]:border data-[current]:rounded-md data-[selected]:rounded-md"
+            onDateChange$={$((date) => {
+              console.log("Date changed:", date);
+              selectedDate.value = date;
+            })}
+          />
+        </Calendar.Grid>
+      </Calendar.Content>
     </Calendar.Root>
   );
 });
--- libs/components/src/calendar/calendar-content.tsx
+++ libs/components/src/calendar/calendar-content.tsx
@@ -1,17 +1,19 @@
-import { Slot, component$ } from "@builder.io/qwik";
+import { Slot, component$, useStyles$ } from "@builder.io/qwik";
 import { useContext } from "@builder.io/qwik";
 import { withAsChild } from "../as-child/as-child";
 import { PopoverContentBase } from "../popover/popover-content";
 import { Render } from "../render/render";
+import styles from "./calendar-content.css?inline";
 import { calendarContextId } from "./calendar-context";
 
 export const CalendarContentBase = component$(() => {
+  useStyles$(styles);
   const context = useContext(calendarContextId);
   const mode = context.mode;
 
   if (mode === "popover") {
     return (
-      <PopoverContentBase>
+      <PopoverContentBase data-qds-calendar-popover-content>
         <Slot />
       </PopoverContentBase>
     );
