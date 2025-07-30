c60e4c6e683731cad63bef2993b7c4af0769e555
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-07-29 19:05:11 -0500

    fix: bundling

--- libs/core/tsdown.config.ts
+++ libs/core/tsdown.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
-  platform: "neutral",
+  entry: ["src/index.ts", "vite/as-child.ts"],
+  external: ["oxc-parser", "magic-string", "@oxc-project/types"],
   dts: {
     isolatedDeclarations: true
   }
