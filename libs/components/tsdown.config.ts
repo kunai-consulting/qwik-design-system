a1ec2e6f2e983833d3b70040359e82ce9667a9eb
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-07-30 23:26:55 -0500

    update externals

--- libs/components/tsdown.config.ts
+++ libs/components/tsdown.config.ts
@@ -10,8 +10,6 @@ export default defineConfig({
   ],
   entry: ["src/**/index.ts"],
   external: [
-    "@qwik.dev/core",
-    "@qwik.dev/core/internal",
     "@playwright/test",
     "@axe-core/playwright",
     /^node:/,
