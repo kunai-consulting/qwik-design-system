d37fc1cf97afe503b4b75a5ce054bcbd62a5cca3
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-08-08 17:43:47 -0500

    feat: improve indicator perf

--- libs/components/src/otp/otp-item-indicator.tsx
+++ libs/components/src/otp/otp-item-indicator.tsx
@@ -1,4 +1,4 @@
-import { type PropsOf, Slot, component$, useContext } from "@qwik.dev/core";
+import { type PropsOf, Slot, component$, useComputed$, useContext } from "@qwik.dev/core";
 import { Render } from "../render/render";
 import { OTPContextId } from "./otp-context";
 import { itemContextId } from "./otp-item";
@@ -7,15 +7,23 @@ import { itemContextId } from "./otp-item";
 export const OtpItemIndicator = component$(({ ...props }: PropsOf<"span">) => {
   const itemContext = useContext(itemContextId);
   const context = useContext(OTPContextId);
-  const isHighlighted =
-    context.currentIndex.value === itemContext.index && context.isFocused.value;
-  const isEmpty = !context.code.value[itemContext.index];
-  const showCaret = isHighlighted && isEmpty;
+
+  const isHighlighted = useComputed$(() => {
+    return context.currentIndex.value === itemContext.index && context.isFocused.value;
+  });
+
+  const isEmpty = useComputed$(() => {
+    return !context.code.value[itemContext.index];
+  });
+
+  const isVisible = useComputed$(() => {
+    return isHighlighted.value && isEmpty.value;
+  });
 
   return (
     // The identifier for the OTP caret element with its specific index
     <Render {...props} fallback="span" data-qds-otp-caret={itemContext.index}>
-      {showCaret && <Slot />}
+      {isVisible.value && <Slot />}
     </Render>
   );
 });
