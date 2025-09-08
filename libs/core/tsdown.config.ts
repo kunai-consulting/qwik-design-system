1b9d6710b433eedb7325d3399af4a4c9c1fa4c12
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-09-08 15:44:53 -0500

    fix: type errors

--- libs/core/tsdown.config.ts
+++ libs/core/tsdown.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["src/index.ts", "vite/index.ts", "utils/index.ts"],
   external: ["oxc-parser", "magic-string", "@oxc-project/types"],
-  dts: {
-    isolatedDeclarations: true
-  }
+  tsconfig: "tsconfig.json",
+  dts: true
 });
