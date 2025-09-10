d1c70ef741d152acad134d72aa42942cf2c5e4d5
thejackshelton-kunaico <jack.shelton@kunaico.com>
2025-09-10 13:51:27 -0500

    add oxc-walker to external

--- libs/core/tsdown.config.ts
+++ libs/core/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts", "vite/index.ts", "utils/index.ts"],
-  external: ["oxc-parser", "magic-string", "@oxc-project/types"],
+  external: ["oxc-parser", "oxc-walker", "magic-string", "@oxc-project/types"],
   tsconfig: "tsconfig.json",
   dts: true
 });
